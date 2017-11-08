41d03a23ed63fdf4d1c1fdfde6ca4557580852f0
Saurabh Vikas Talele <saurabh.talele+github@gmail.com>
2017-11-08 12:11:43 +0900

    Add files via upload

---
+++ LICENSE.md/Luria_Delbruck_V3.m
@@ -0,0 +1,38 @@
+%% Luria Delbruck Experiment 
+clc
+clear all
+
+%% Initialize the setup
+
+tot_time = 1000;
+iter = 10000; %iterations of experiments
+p_mut = 0.005; %probability of mutation
+
+avg_time_for_division = 100;
+
+
+
+global var1;
+global var2;
+
+%% Cell Division
+
+for t = 1: tot_time
+
+    tot_pop = 0;
+    mut_pop = 0;
+    
+    
+    
+    
+end
+
+%% 
+function go(cur_pop , cur_mut , p_mut , cur_time)
+
+    global var1;
+    global var2;
+    
+    r = t + poissrnd(50);
+
+end 
